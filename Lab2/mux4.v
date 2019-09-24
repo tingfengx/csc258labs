81413d20e483fa559442a6cf5553df21e5e92a0e
TINGFENG XIA <tingfeng.xia@outlook.com>
2019-09-24 18:57:19 -0400

    reformat file

--- Lab2/mux4.v
+++ Lab2/mux4.v
@@ -6,27 +6,27 @@
 module mux4(LEDR, SW);
     input [9:0] SW;
     output [9:0] LEDR;
-	 // declare two wires that connects mux2to1's
+	  // declare two wires that connects mux2to1's
     wire w1;
-	wire w2;
+	  wire w2;
 	 // Left top mux2to1
-    mux2to1 u1(
-	    .x(SW[0]),
+   mux2to1 u1(
+    .x(SW[0]),
 		.y(SW[2]),
 		.s(SW[9]),
-		.m(w1));
+    .m(w1));
 	 // left bottom mux2to1
 	 mux2to1 u2(
-        .x(SW[1]),
-		.y(SW[3]),
-		.s(SW[9]),
-		.m(w2));
-	 // right mux2to1
+    .x(SW[1]),
+    .y(SW[3]),
+    .s(SW[9]),
+    .m(w2));
+   // right mux2to1
 	 mux2to1 u3(
-		.x(w1),
-		.y(w2),
-		.s(SW[8]),
-		.m(LEDR[0])); // final output
+    .x(w1),
+    .y(w2),
+    .s(SW[8]),
+    .m(LEDR[0])); // final output
 endmodule
 
 // This is borrowed from the mux.v file available on quercus
@@ -35,7 +35,7 @@ module mux2to1(x, y, s, m);
     input y; //selected when s is 1
     input s; //select signal
     output m; //output
-  
+
     assign m = s & y | ~s & x;
     // OR
     // assign m = s ? y : x;
